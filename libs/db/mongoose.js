e81879b4dd7f7af8436ad97ba80aaf5a55fd1f1d
Raja <37906074+ImRajja@users.noreply.github.com>
2022-07-01 15:41:11 +0530

    Env variable

--- libs/db/mongoose.js
+++ libs/db/mongoose.js
@@ -1,20 +1,22 @@
-var mongoose = require('mongoose');
+var mongoose = require("mongoose");
+require("dotenv").config();
 
-var libs = process.cwd() + '/libs/';
+var libs = process.cwd() + "/libs/";
 
-var log = require(libs + 'log')(module);
-var config = require(libs + 'config');
+var log = require(libs + "log")(module);
+var config = require(libs + "config");
 
-mongoose.connect(config.get('mongoose:uri'));
+// mongoose.connect(config.get('mongoose:uri'));
+mongoose.connect(process.env.MONGODB_URI);
 
 var db = mongoose.connection;
 
-db.on('error', function (err) {
-    log.error('Connection error:', err.message);
+db.on("error", function (err) {
+  log.error("Connection error:", err.message);
 });
 
-db.once('open', function callback() {
-    log.info('Connected to DB!');
+db.once("open", function callback() {
+  log.info("Connected to DB!");
 });
 
 module.exports = mongoose;
